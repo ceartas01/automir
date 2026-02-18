c3409828ff646697bbe464297a582192a2b7fa2c
Рамиль <ramilvagizov020@gmail.com>
2026-02-18 11:46:54 +0300

    TODO 1: Добавлено свойство Category для запчасти

--- AutoPart.cs
+++ AutoPart.cs
@@ -1,22 +1,18 @@
-// TODO:
-// 1. Добавить свойство для категории запчасти (двигатель, ходовая, электрика)
-// 2. Реализовать проверку корректности данных (цена, срок гарантии)
-// 3. Реализовать информативное строковое представление запчасти
-
 namespace AutoPartsStore
 {
     public class AutoPart
     {
-        public int Id { get; set; }                    // Артикул
-        public string Name { get; set; }               // Название
-        public string CarBrand { get; set; }           // Марка авто (Toyota, Volkswagen, Lada)
-        public string CarModel { get; set; }           // Модель авто
-        public int ProductionYear { get; set; }        // Год выпуска авто
-        public decimal Price { get; set; }             // Цена
-        public int StockQuantity { get; set; }         // Количество на складе
-        public int WarrantyMonths { get; set; }        // Гарантия в месяцах
-        
-        // TODO 1: Добавить свойство Category (категория: двигатель, тормоза, подвеска, электрика, кузов)
+        public int Id { get; set; }                    
+        public string Name { get; set; }               
+        public string CarBrand { get; set; }           
+        public string CarModel { get; set; }           
+        public int ProductionYear { get; set; }        
+        public decimal Price { get; set; }             
+        public int StockQuantity { get; set; }         
+        public int WarrantyMonths { get; set; }        
+
+        // TODO 1: Добавлено свойство Category
+        public string Category { get; set; }          
         
         public AutoPart(int id, string name, string brand, string model, int year, decimal price, int stock, int warranty, string category)
         {
@@ -26,36 +22,27 @@ namespace AutoPartsStore
             CarModel = model;
             ProductionYear = year;
             
-            // TODO 2: Проверить что цена не отрицательная
-            // Если цена < 0, установить минимальную цену 100
-            
             StockQuantity = stock;
             
-            // TODO 2: Проверить что гарантия не отрицательная
-            // Если warranty < 0, установить 0
-            
-            // TODO 1: Сохранить категорию запчасти
+            // TODO 1: Сохранение категории
+            Category = category;
         }
         
         public override string ToString()
         {
-            // TODO 3: Вернуть строку в формате "Тормозной диск Toyota Corolla 2015 (тормоза) - 3200 руб. (12 мес гарантии)"
             return Name;
         }
         
-        // Проверить совместимость с автомобилем
         public bool IsCompatible(string brand, string model, int year)
         {
             return CarBrand == brand && CarModel == model && ProductionYear == year;
         }
         
-        // Проверить наличие на складе
         public bool IsInStock(int quantity = 1)
         {
             return StockQuantity >= quantity;
         }
         
-        // Продать запчасть (уменьшить остаток)
         public bool Sell(int quantity)
         {
             if (StockQuantity >= quantity)
